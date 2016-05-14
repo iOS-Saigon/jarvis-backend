55efd0a6d3b3f6dea0919ecd84b1a024b463ed6a
Nisanth Chunduru <nisanth074@gmail.com>
2016-05-14 17:53:49 +0700

    Add timestamps to tables. Rename showtime as screening.

--- scrape_dimovie.rb
+++ scrape_dimovie.rb
@@ -17,22 +17,30 @@ ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "./titan.s
 ActiveRecord::Schema.define do
   create_table :movies do |table|
     table.column :name, :string
+
+    table.timestamps null: false
   end
 
   create_table :theaters do |table|
     table.column :name, :string
     table.column :district, :string
     table.column :brand_id, :integer
+
+    table.timestamps null: false
   end
 
   create_table :brands do |table|
     table.column :name, :string
+
+    table.timestamps null: false
   end
 
   create_table :screenings do |table|
     table.column :screening_at, :datetime
     table.column :movie_id, :integer
     table.column :theater_id, :integer
+
+    table.timestamps null: false
   end
 end
 
@@ -54,13 +62,13 @@ end
 class Movie < ActiveRecord::Base
 end
 
-class ShowtimesPage
+class ScreeningsPage
   attr_reader :movie_name
   attr_reader :url
 
-  def initialize(movie_name, movie_showtime_url)
+  def initialize(movie_name, movie_screening_url)
     @movie_name = movie_name
-    @url = movie_showtime_url
+    @url = movie_screening_url
   end
 
   def scrape
@@ -72,7 +80,7 @@ class ShowtimesPage
     25.times do |i|
       scroll_position = 5000 * i
       driver.execute_script("window.scrollTo(0, #{scroll_position})")
-      sleep 2 # Wait for some time for the showtime data to be fetched
+      sleep 2 # Wait for some time for the screening data to be fetched
       begin
         driver.find_element(:xpath, %Q(//div[@date="#{date}"]))
       rescue Selenium::WebDriver::Error::NoSuchElementError
@@ -81,21 +89,21 @@ class ShowtimesPage
       break
     end
 
-    showtimes_wrapper_element = driver.find_element(:id => "wrap-time")
-    showtime_elements = showtimes_wrapper_element.find_elements(:xpath, %Q(//div[@class="item-session future clearfix"]))
+    screenings_wrapper_element = driver.find_element(:id => "wrap-time")
+    screening_elements = screenings_wrapper_element.find_elements(:xpath, %Q(//div[@class="item-session future clearfix"]))
 
     movie = Movie.create!(name: movie_name)
 
-    showtime_elements.each do |showtime_element|
-      showtime_info = showtime_element.text.split("\n") # Sun, 21:00, 2D, LOTTE | D.2, Cantavil An Phu, English Vietnamese Sub, No booking
+    screening_elements.each do |screening_element|
+      screening_info = screening_element.text.split("\n") # Sun, 21:00, 2D, LOTTE | D.2, Cantavil An Phu, English Vietnamese Sub, No booking
 
-      day = showtime_info[0] # Sun
-      time = showtime_info[1] # 21:00
+      day = screening_info[0] # Sun
+      time = screening_info[1] # 21:00
       screening_at = Chronic.parse("#{day}, #{time}").utc
 
-      theater_brand_and_district = showtime_info[3] # LOTTE | D.2
+      theater_brand_and_district = screening_info[3] # LOTTE | D.2
       theater_brand, theater_district = theater_brand_and_district.split(" | ")
-      theater_name = showtime_info[4]
+      theater_name = screening_info[4]
 
       brand = Brand.find_or_create_by(name: theater_brand)
       theater = Theater.find_or_create_by(name: theater_name) # Assuming that each theater has a unique name for now
@@ -112,7 +120,7 @@ class ShowtimesPage
   end
 end
 
-movie_name_movie_showtime_url_mapping = {
+movie_name_movie_screening_url_mapping = {
   "Angry Birds" => "http://dimovie.vn/en/sessions?fid=20601",
   # "Captain America: Civil War" => "http://dimovie.vn/en/sessions?fid=18882",
   # "Ratchet & Clank" => "http://dimovie.vn/en/sessions?fid=23456",
@@ -124,7 +132,7 @@ driver = Selenium::WebDriver.for(:firefox)
 $driver = driver
 driver.manage.window.maximize
 $date = "2016-05-17" # Tuesday
-movie_name_movie_showtime_url_mapping.each do |movie_name, movie_showtime_url|
-  ShowtimesPage.new(movie_name, movie_showtime_url).scrape
+movie_name_movie_screening_url_mapping.each do |movie_name, movie_screening_url|
+  ScreeningsPage.new(movie_name, movie_screening_url).scrape
 end
 driver.quit
