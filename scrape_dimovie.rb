f567636676e1072cf82d0ea7c3e2ef58b3c87743
Nisanth Chunduru <nisanth074@gmail.com>
2016-05-14 19:15:25 +0700

    Scrape movie information of currently running movies

--- scrape_dimovie.rb
+++ scrape_dimovie.rb
@@ -17,6 +17,10 @@ ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "./titan.s
 ActiveRecord::Schema.define do
   create_table :movies do |table|
     table.column :name, :string
+    table.column :synopsis, :string
+    table.column :duration, :integer
+    table.column :dimovie_poster_url, :string
+    table.column :vietnamese_release_at, :datetime
 
     table.timestamps null: false
   end
@@ -89,10 +93,10 @@ class ScreeningsPage
       break
     end
 
+    movie = Movie.find_by_name(movie_name)
+
     screenings_wrapper_element = driver.find_element(:id => "wrap-time")
     screening_elements = screenings_wrapper_element.find_elements(:xpath, %Q(//div[@class="item-session future clearfix"]))
-
-    movie = Movie.create!(name: movie_name)
 
     screening_elements.each do |screening_element|
       screening_info = screening_element.text.split("\n") # Sun, 21:00, 2D, LOTTE | D.2, Cantavil An Phu, English Vietnamese Sub, No booking
@@ -120,19 +124,80 @@ class ScreeningsPage
   end
 end
 
-movie_name_movie_screening_url_mapping = {
-  "Angry Birds" => "http://dimovie.vn/en/sessions?fid=20601",
-  # "Captain America: Civil War" => "http://dimovie.vn/en/sessions?fid=18882",
-  # "Ratchet & Clank" => "http://dimovie.vn/en/sessions?fid=23456",
-  # "Tracer" => "http://dimovie.vn/en/sessions?fid=24525",
-  # "The Jungle Book" => "http://dimovie.vn/en/sessions?fid=21551",
-  "Lật Mặt" => "http://dimovie.vn/en/sessions?fid=18302"
-}
+class MoviePage
+  attr_reader :movie_name
+  attr_reader :url
+
+  def initialize(movie_name, movie_page_url)
+    @movie_name = movie_name
+    @url = movie_page_url
+  end
+
+  def scrape
+    driver = $driver
+
+    driver.navigate.to(url)
+
+    # name = driver.find_element(:xpath, %Q(//div[@class="views-field views-field-title-field"])).text
+    synopsis = driver.find_element(:xpath, %Q(//div[contains(@class, "view-movie-synopsis")])).text.sub("Play Trailer\n", "")
+    # Duration may be unavailable sometimes
+    duration_in_minutes = driver.find_element(:xpath, %Q(//div[contains(@class, "views-field-field-duration")])).text.scan(/\d/).join("").to_i # "Duration\n95 min"
+    dimovie_poster_url = driver.find_element(:xpath, %Q(//img[contains(@class, "image-style-poster-large")])).attribute(:src)
+    vietnamese_release_date_string = driver.find_element(:xpath, %Q(//div[contains(@class, "views-field-field-vn-release-date")])).text.split(" ")[-1] # "Vietnamese release 13.05.16"
+    vietnamese_release_at = Time.strptime(vietnamese_release_date_string, "%d.%m.%y").utc
+
+    movie = Movie.new
+    movie.name = movie_name
+    movie.synopsis = synopsis
+    movie.duration = duration_in_minutes * 60 unless duration_in_minutes.zero?
+    movie.dimovie_poster_url = dimovie_poster_url
+    movie.vietnamese_release_at = vietnamese_release_at
+    movie.save!
+  end
+end
+
 driver = Selenium::WebDriver.for(:firefox)
 $driver = driver
 driver.manage.window.maximize
 $date = "2016-05-17" # Tuesday
-movie_name_movie_screening_url_mapping.each do |movie_name, movie_screening_url|
-  ScreeningsPage.new(movie_name, movie_screening_url).scrape
+currently_running_movies = [
+  {
+    "name" => "Angry Birds",
+    "screenings_page_url" => "http://dimovie.vn/en/sessions?fid=20601",
+    "url" => "http://dimovie.vn/en/movies/angry-birds"
+  },
+  {
+    "name" => "Captain America: Civil War",
+    "screenings_page_url" => "http://dimovie.vn/en/sessions?fid=18882",
+    "url" => "http://dimovie.vn/en/movies/captain-america-civil-war"
+  },
+  {
+    "name" => "Ratchet & Clank",
+    "screenings_page_url" => "http://dimovie.vn/en/sessions?fid=23456",
+    "url" => "http://dimovie.vn/en/movies/ratchet-clank"
+  },
+  {
+    "name" => "Tracer",
+    "screenings_page_url" => "http://dimovie.vn/en/sessions?fid=24525",
+    "url" => "http://dimovie.vn/en/movies/tracer"
+  },
+  {
+    "name" => "The Jungle Book",
+    "screenings_page_url" => "http://dimovie.vn/en/sessions?fid=21551",
+    "url" => "http://dimovie.vn/en/movies/jungle-book"
+  },
+  {
+    "name" => "Lật Mặt",
+    "screenings_page_url" => "http://dimovie.vn/en/sessions?fid=18302",
+    "url" => "http://dimovie.vn/en/movies/lat-mat"
+  }
+]
+currently_running_movies.each do |movie_hash|
+  movie_name = movie_hash["name"]
+  movie_page_url = movie_hash["url"]
+  screenings_page_url = movie_hash["screenings_page_url"]
+
+  MoviePage.new(movie_name, movie_page_url).scrape
+  ScreeningsPage.new(movie_name, screenings_page_url).scrape
 end
 driver.quit
