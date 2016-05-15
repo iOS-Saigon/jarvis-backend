e15bfb46edcf4f8d5842f1fc3adbb6193bb7be05
Nisanth Chunduru <nisanth074@gmail.com>
2016-05-15 16:51:46 +0700

    Fetching screening information from a database

--- api.rb
+++ api.rb
@@ -3,6 +3,29 @@ require "pry"
 require "wit"
 require "chronic"
 require "yaml"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "./titan.sqlite3")
+
+class Movie < ActiveRecord::Base
+end
+
+class Brand < ActiveRecord::Base
+end
+
+class Theater < ActiveRecord::Base
+  belongs_to :brand
+end
+
+class Screening < ActiveRecord::Base
+  belongs_to :movie
+  belongs_to :theater
+end
+
+class Movie < ActiveRecord::Base
+  has_many :screenings
+  has_many :theaters, through: :screenings
+end
 
 WIT_ACCESS_TOKEN = "QZJFZIMIS4UIL4H533E2KFEMX76JU4HV"
 
@@ -62,12 +85,22 @@ class WitResponseHandler
     session = Session.new
 
     case response.intent
+    when "greet"
+      "Hi there!"
     when "tell_movie_name"
       session["movie_name"] = response.movie_name
       "That's a great choice! When do you want to see it?"
     when "tell_preferred_screening_time"
-      session["preferred_screening_time"] = Chronic.parse(response.datetime)
+      session["preferred_screening_time"] = Chronic.parse(response.datetime).utc
       "Alright, I've booked a ticket for #{session['movie_name']} at that time. Have a great day!"
+
+      movie = Movie.find_by_name(session["movie_name"])
+      preferred_screening_time = session["preferred_screening_time"]
+      screening = movie.screenings.where("screening_at > ?", preferred_screening_time).order(screening_at: :asc).first
+      theater = screening.theater
+      screening_at_in_vietnam_time = screening.screening_at + (7 * 3600)
+
+      "Alright, I've booked a ticket for #{session['movie_name']} at #{theater.name} at #{screening_at_in_vietnam_time.strftime("%H:%M %p")}. Have a great day!"
     else
       "Sorry, I don't understand you :("
     end
@@ -89,11 +122,17 @@ def wit_client
 end
 
 get "/reply" do
-  message_text = params[:text]
+  begin
+    message_text = params[:text]
 
-  wit_response_hash = wit_client.message(message_text)
-  wit_response = WitResponse.new(wit_response_hash)
-  reply_text = WitResponseHandler.new.handle_response(wit_response)
+    wit_response_hash = wit_client.message(message_text)
+    wit_response = WitResponse.new(wit_response_hash)
+    reply_text = WitResponseHandler.new.handle_response(wit_response)
+  rescue => e
+    puts e.message
+    puts e.backtrace
+    reply_text = "Oops! Something went wrong. Please type again."
+  end
 
   # { reply: { text: reply_text } }.to_json
   { reply: reply_text }.to_json
